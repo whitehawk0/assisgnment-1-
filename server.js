e496c626c2c0d25e11100e0dd444409ede4b2d1d
Jatin <122469398+whitehawk0@users.noreply.github.com>
2023-09-06 15:20:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Jatin Student ID: 141358218 Date: 07/Sep/2023
+*
+* Online (Cyclic) URL: https://weak-tan-goose-garb.cyclic.app
+*
+********************************************************************************/
+
 var express = require("express")
 var app = express();
 
